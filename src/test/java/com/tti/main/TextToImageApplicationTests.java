65c01e5eced14a20309783cd942a6778909122f7
Den of Projects <rahul.mrrv@gmail.com>
2025-02-02 10:29:35 +0530

    shree ganeshay namah

---
+++ src/test/java/com/tti/main/TextToImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tti.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
